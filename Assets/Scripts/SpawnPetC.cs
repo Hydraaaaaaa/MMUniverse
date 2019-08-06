fb99c678c1f58e716d7b28448295a2f0f9e164b6
Ritor1 <azamatov-19@mail.ru>
2019-08-06 23:22:18 +0600

    Monster attack/Player attack/Game Over

---
+++ Assets/Scripts/SpawnPetC.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BulletStatusC))]
+
+public class SpawnPetC : MonoBehaviour {
+	public GameObject petPrefab;
+	public float additionY = 0.3f;
+	
+	void  Start (){
+		GameObject source = GetComponent<BulletStatusC>().shooter;
+		if(source.GetComponent<AttackTriggerC>().pet){
+			source.GetComponent<AttackTriggerC>().pet.GetComponent<StatusC>().Death();
+		}
+		Vector3 spawnPoint = transform.position;
+		spawnPoint.y += additionY;
+		GameObject pet = Instantiate(petPrefab , spawnPoint , source.transform.rotation) as GameObject;
+		pet.GetComponent<AIfriendC>().master = source.transform;
+		source.GetComponent<AttackTriggerC>().pet = pet;
+		Destroy(gameObject);
+	}
+
+}
